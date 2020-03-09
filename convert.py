e31c5d86af4f0fccb93e1afeb8aae24dda153540
Av <ayaanvirani0@gmail.com>
2020-03-09 19:28:44 +0000

    Create convert.py

---
+++ convert.py
@@ -0,0 +1,12 @@
+def decimalToBinary(num):
+    """This function converts decimal number
+    to binary and prints it"""
+    if num > 1:
+        decimalToBinary(num // 2)
+    print(num % 2, end='')
+
+
+number = int(input("Enter any decimal number: "))
+print("Output:")
+decimalToBinary(number)
+print("\n")
